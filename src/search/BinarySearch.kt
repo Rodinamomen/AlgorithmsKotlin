50db2b270e2fbc44c8851c2115e2b45342320298
Rodinamomen <Rodinamobark3@gmail.com>
2026-02-25 01:51:01 +0200

    Binary search

---
+++ src/search/BinarySearch.kt
@@ -0,0 +1,21 @@
+package search
+
+fun search(nums: IntArray, target: Int): Int {
+    var high = nums.lastIndex
+    var low = 0
+    while (low <= high) {
+        val mid = ((low + high) / 2)
+        if(nums[mid] == target) {
+            return mid
+        }else if(nums[mid]>target){
+            high = mid - 1
+        }else{
+            low = mid + 1
+        }
+    }
+    return -1
+}
+
+fun main(){
+   println(search(intArrayOf(1,2,3,4,5,6,7,8,9,10), 3))
+}
